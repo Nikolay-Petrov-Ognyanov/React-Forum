4787b2656f6378841b340af1c9a9214655bbdb58
Nikolay-Petrov-Ognyanov <nikolaypetrovognyanov25051995@gmail.com>
2023-05-13 14:51:12 +0300

    add authentication error handling

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -1,64 +1,121 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import * as service from "../service"
 import { Context } from "../Context"
+import { useNavigate } from "react-router-dom"
 
 export function Auth() {
-    const { users, setUsers, user, setUser } = useContext(Context)
+    const {
+        users,
+        setUsers,
+        user,
+        setUser
+    } = useContext(Context)
 
-    async function handleRegister(event) {
+    const [isRegistering, setIsRegistering] = useState(true)
+    const [modalMessage, setModalMessage] = useState("")
+    const [showModal, setShowModal] = useState(false)
+
+    const navigate = useNavigate()
+
+    function handleRegister() {
+        setIsRegistering(true)
+    }
+
+    function handleLogin() {
+        setIsRegistering(false)
+    }
+
+    async function handleSubmit(event) {
         event.preventDefault()
 
         const formData = Object.fromEntries(new FormData(event.target))
 
         if (!Object.values(formData).some(v => !v)) {
+            let result
 
-            service.register(formData).then(result => {
-                if (!result.messagex) {
+            try {
+                if (isRegistering) {
+                    result = await service.register(formData)
+                } else {
+                    result = await service.login(formData)
+                }
+
+                if (result && !result.message) {
                     setUser(result)
 
                     for (let key in result) {
                         localStorage.setItem(key, result[key])
                     }
+
+                    navigate("/")
+                } else {
+                    if (Array.isArray(result.message)) {
+                        let message = result.message[0]
+
+                        if (result.message.length === 2) {
+                            let passwordMessage = result.message[1].toLowerCase()
+
+                            message = `${result.message[0]} and ${passwordMessage}`
+                        }
+
+                        result.message = message
+                    }
+
+                    setModalMessage(result.message)
+                    setShowModal(true)
                 }
-            }).catch(error => console.error(error))
+            } catch (error) {
+                console.error(error)
+            }
         }
     }
 
-    async function handleLogin(event) {
-        event.preventDefault()
-
-        const formData = Object.fromEntries(new FormData(event.target))
-
-        if (!Object.values(formData).some(v => !v)) {
-            service.login(formData).then(result => {
-                console.log(result)
-
-                if (!result.message) {
-                    setUser(result)
-
-                    for (let key in result) {
-                        localStorage.setItem(key, result[key])
-                    }
-                }
-            }).catch(error => console.error(error))
-        }
+    function closeModal() {
+        setModalMessage("")
+        setShowModal(false)
     }
 
     return (
         <section className="auth">
-            <form onSubmit={handleRegister}>
-                <input type="text" name="username" placeholder="Username" />
-                <input type="password" name="password" placeholder="Password" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                />
 
-                <button>Register</button>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                />
+
+                <div className="buttonsWrapper">
+                    <button
+                        type="submit"
+                        onClick={handleRegister}
+                    >
+                        Register
+                    </button>
+
+                    <button
+                        type="submit"
+                        onClick={handleLogin}
+                    >
+                        Login
+                    </button>
+                </div>
             </form>
 
-            <form onSubmit={handleLogin}>
-                <input type="text" name="username" placeholder="Username" />
-                <input type="password" name="password" placeholder="Password" />
+            {showModal &&
+                <div className="modal">
+                    <div className="modalMessage">
+                        {modalMessage}
+                    </div>
 
-                <button>Login</button>
-            </form>
+                    <button onClick={closeModal}>OK</button>
+                </div>
+            }
         </section>
     )
 }
